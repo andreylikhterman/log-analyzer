7d2a6bfeadc1c3157e6685fb334310648c1ff51e
andreylikhterman <andreylikhterman@gmail.com>
2024-11-17 11:03:44 +0300

    fixed formatter: extra spaces have been removed

--- internal/application/formatter/markdown/markdown.go
+++ internal/application/formatter/markdown/markdown.go
@@ -12,26 +12,26 @@ type Formatter struct{}
 
 func (w *Formatter) WriteGeneralInfo(builder *strings.Builder, report *domain.LogReport) {
 	builder.WriteString("## Общая информация\n\n")
-	builder.WriteString("| **Метрика**                     | **Значение**              |\n")
-	builder.WriteString("|---------------------------------|---------------------------|\n")
+	builder.WriteString("| **Метрика** | **Значение** |\n")
+	builder.WriteString("|:---------------------------------|:---------------------------|\n")
 
 	w.writeFileOrURLNames(builder, report.FileNames, report.URLName)
 	w.writeDate(builder, "Начальная дата", report.StartDate)
 	w.writeDate(builder, "Конечная дата", report.EndDate)
-	fmt.Fprintf(builder, "| Количество запросов             | %-25s |\n", output.FormatNumber(report.TotalRequests))
-	fmt.Fprintf(builder, "| Средний размер ответа           | %-25s |\n", fmt.Sprintf("%sb", output.FormatNumber(report.AvgBodySize)))
-	fmt.Fprintf(builder, "| 95p размера ответа              | %-25s |\n", fmt.Sprintf("%sb", output.FormatNumber(report.Percentile95Size)))
-	fmt.Fprintf(builder, "| Среднее время между запросами   | %-25s |\n", report.AvgTimeBetweenRequests)
+	fmt.Fprintf(builder, "| Количество запросов | %s |\n", output.FormatNumber(report.TotalRequests))
+	fmt.Fprintf(builder, "| Средний размер ответа | %sb |\n", output.FormatNumber(report.AvgBodySize))
+	fmt.Fprintf(builder, "| 95p размера ответа | %sb |\n", output.FormatNumber(report.Percentile95Size))
+	fmt.Fprintf(builder, "| Среднее время между запросами | %s |\n", report.AvgTimeBetweenRequests)
 	builder.WriteString("\n")
 }
 
 func (w *Formatter) WriteRequestedResources(builder *strings.Builder, report *domain.LogReport) {
 	builder.WriteString("## Запрашиваемые ресурсы\n\n")
-	builder.WriteString("| **Ресурс**             | **Количество**            |\n")
-	builder.WriteString("|------------------------|---------------------------|\n")
+	builder.WriteString("| **Ресурс** | **Количество** |\n")
+	builder.WriteString("|:------------------------|:---------------------------|\n")
 
 	for _, resource := range report.SortedRequestedResources[:min(3, len(report.RequestedResources))] {
-		fmt.Fprintf(builder, "| %-22s | %-25s |\n", fmt.Sprintf("`%s`", resource), output.FormatNumber(report.RequestedResources[resource]))
+		fmt.Fprintf(builder, "| `%s` | %s |\n", resource, output.FormatNumber(report.RequestedResources[resource]))
 	}
 
 	builder.WriteString("\n")
@@ -39,11 +39,11 @@ func (w *Formatter) WriteRequestedResources(builder *strings.Builder, report *do
 
 func (w *Formatter) WriteResponseCodes(builder *strings.Builder, report *domain.LogReport) {
 	builder.WriteString("## Коды ответа\n\n")
-	builder.WriteString("|**Код**| **Имя**               | **Количество**      |\n")
-	builder.WriteString("|-------|-----------------------|---------------------|\n")
+	builder.WriteString("|**Код**| **Имя** | **Количество** |\n")
+	builder.WriteString("|:-------|:-----------------------|:---------------------|\n")
 
 	for _, code := range report.SortedResponseCodes[:min(3, len(report.ResponseCodes))] {
-		fmt.Fprintf(builder, "| %-5d | %-21s | %-19s |\n",
+		fmt.Fprintf(builder, "| %d | %s | %s |\n",
 			code, report.ResponseCodes[code].Name, output.FormatNumber(report.ResponseCodes[code].Count))
 	}
 
@@ -56,11 +56,11 @@ func (w *Formatter) WriteTopIPAddresses(builder *strings.Builder, report *domain
 	}
 
 	builder.WriteString("## Топ IP-адресов\n\n")
-	builder.WriteString("| **IP-адрес**          | **Количество запросов**   |\n")
-	builder.WriteString("|-----------------------|---------------------------|\n")
+	builder.WriteString("| **IP-адрес** | **Количество запросов** |\n")
+	builder.WriteString("|:-----------------------|:---------------------------|\n")
 
 	for _, ipCount := range report.TopIPAddresses {
-		fmt.Fprintf(builder, "| %-21s | %-25s |\n", ipCount.IP, output.FormatNumber(ipCount.Count))
+		fmt.Fprintf(builder, "| %s | %s |\n", ipCount.IP, output.FormatNumber(ipCount.Count))
 	}
 
 	builder.WriteString("\n")
@@ -68,23 +68,23 @@ func (w *Formatter) WriteTopIPAddresses(builder *strings.Builder, report *domain
 
 func (w *Formatter) writeFileOrURLNames(builder *strings.Builder, fileNames []string, urlName string) {
 	if len(fileNames) == 0 {
-		fmt.Fprintf(builder, "| URL                             | %-25s |\n", "`"+urlName+"`")
+		fmt.Fprintf(builder, "| URL | %s |\n", "`"+urlName+"`")
 		return
 	}
 
 	for i, fileName := range fileNames {
 		if i == 0 {
-			fmt.Fprintf(builder, "| Файл(-ы)                        | %-25s |\n", "`"+fileName+"`")
+			fmt.Fprintf(builder, "| Файл(-ы) | %s |\n", "`"+fileName+"`")
 		} else {
-			fmt.Fprintf(builder, "|                                 | %-25s |\n", "`"+fileName+"`")
+			fmt.Fprintf(builder, "| | %s |\n", "`"+fileName+"`")
 		}
 	}
 }
 
 func (w *Formatter) writeDate(builder *strings.Builder, label string, date time.Time) {
 	if date.IsZero() {
-		fmt.Fprintf(builder, "| %-31s | %-25s |\n", label, "-")
+		fmt.Fprintf(builder, "| %s | %s |\n", label, "-")
 	} else {
-		fmt.Fprintf(builder, "| %-31s | %-25s |\n", label, date.Format("02.01.2006"))
+		fmt.Fprintf(builder, "| %s | %s |\n", label, date.Format("02.01.2006"))
 	}
 }
